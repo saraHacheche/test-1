44c2b6536d9c2e36b220f5dd33a33108225a8faf
sara hacheche <hachechesara@gmail.com>
2023-08-14 23:06:35 -0400

    _allo

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -138,6 +138,7 @@ describe('example to-do app', () => {
 
       // Finally, make sure that the clear button no longer exists.
       cy.contains('Clear completed').should('not.exist')
+      //allo samir
     })
   })
 })
